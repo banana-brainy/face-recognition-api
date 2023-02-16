a0ab0c4f1f60e40bce3892351080f9245c6ef5ca
banana-brainy <krokodeall124@gmail.com>
2023-02-16 13:11:05 +0200

    adding devoloping working state of my api

---
+++ dist/index.js
@@ -0,0 +1,98 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+/* import bcrypt, { hash } from 'bcryptjs'; */
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+};
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
+app.post('/signin', (req, res) => {
+    /* bcrypt.compare(password, hash, function(err, res) {
+      <your code here>
+    }); */
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    }
+    else {
+        res.status(400).json('error logging in');
+    }
+});
+app.post('/register', (req, res) => {
+    const { email, name } = req.body;
+    /* bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+      });
+    }); */
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date(),
+    });
+    res.json(database.users[database.users.length - 1]);
+    // grabs the last item in the array
+    // which is the one that we've added with .push
+    // so this adds a new user to the database
+});
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    });
+    if (!found) {
+        res.status(400).json('not found');
+    }
+});
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+    if (!found) {
+        res.status(400).json('not found');
+    }
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ index.ts
@@ -0,0 +1,114 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+/* import bcrypt, { hash } from 'bcryptjs'; */
+import cors from 'cors';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+app.use(bodyParser.json());
+app.use(cors());
+
+interface IUserForDatabase {
+  id: string;
+  name: string;
+  email: string;
+  password?: string;
+  entries: number;
+  joined: Date;
+}
+
+interface IDatabase {
+  users: IUserForDatabase[];
+}
+
+const database: IDatabase = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req: Request, res: Response) => {
+  /* bcrypt.compare(password, hash, function(err, res) {
+    <your code here>
+  }); */
+  if (req.body.email === database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json('success');
+      } else {
+        res.status(400).json('error logging in');
+      }
+})
+
+app.post('/register', (req: Request, res: Response) => {
+  const { email, name }: IUserForDatabase = req.body;
+  /* bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+    });
+  }); */
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  })
+  res.json(database.users[database.users.length-1])
+  // grabs the last item in the array
+  // which is the one that we've added with .push
+  // so this adds a new user to the database
+})
+
+app.get('/profile/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  let found: boolean = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found')
+  }
+})
+
+app.put('/image', (req: Request, res: Response) => {
+  const { id } = req.body;
+  let found: boolean = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  })
+  if (!found) {
+    res.status(400).json('not found')
+  }
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
