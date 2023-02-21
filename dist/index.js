9cf751e22d203df43033c3d51de2147feaf59f01
banana-brainy <krokodeall124@gmail.com>
2023-02-21 17:48:03 +0200

    made the full routing possible

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const knex_1 = __importDefault(require("knex"));
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 // Connecting to my db using knex.
 const db = (0, knex_1.default)({
     client: 'pg',
@@ -23,64 +24,53 @@ const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
-// This is going away soon.
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-};
-app.get('/', (req, res) => {
-    res.send(database.users);
-});
+// 
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    }
-    else {
-        res.status(400).json('error logging in');
-    }
+    return db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {
+        const isValid = bcryptjs_1.default.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            db.select('*').from('users')
+                .where('email', '=', req.body.email)
+                .then(user => {
+                res.json(user[0]);
+            })
+                .catch(err => res.status(400).json('unable to get a user'));
+        }
+        else {
+            res.status(400).json('wrong credentials');
+        }
+    })
+        .catch(err => res.status(400).json('wrong credentials'));
 });
-/*
-.insert({
-  // If you are using Knex.js version 1.0.0 or higher this
-  // now returns an array of objects. Therefore, the code goes from:
-  // loginEmail[0] --> this used to return the email
-  // TO
-  // loginEmail[0].email --> this now returns the email
-     email: loginEmail[0].email, // <-- this is the only change!
-     name: name,
-     joined: new Date()
-})
-*/
-// This route is registering the user and making a call to the db,
+// This route is registering the user and making a call to the DB,
 // checking whether the user is already registered.
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    })
-        .then(user => {
-        res.json(user[0]);
+    const salt = bcryptjs_1.default.genSaltSync(10);
+    const hash = bcryptjs_1.default.hashSync(password, salt);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+                .then(user => {
+                res.json(user[0]);
+            });
+        })
+            .then(trx.commit)
+            .catch(trx.rollback);
     })
         .catch(err => res.status(400).json('unable to register'));
 });
@@ -99,8 +89,6 @@ app.get('/profile/:id', (req, res) => {
     })
         .catch(err => res.status(400).json('Not found'));
 });
-// Why does req.params return a variable with a distinct type
-// and req.body says that its type is 'any'?
 // Updates the rank and increases the count.
 app.put('/image', (req, res) => {
     const { id } = req.body;
--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 import knex from 'knex';
 import bcrypt from 'bcryptjs';
 
-// Connecting to my db using knex.
+// Connecting to my DB using knex.
 const db = knex({
   client: 'pg',
   connection: {
@@ -23,85 +23,62 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cors());
 
-// What will happen with { id }, when I delete this interface?
 interface IUserForDatabase {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  entries: number;
-  joined: Date;
+  id: string,
+  name: string,
+  email: string,
+  password: string,
 }
 
-interface IDatabase {
-  users: IUserForDatabase[];
-}
-
-// This is going away soon.
-const database: IDatabase = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(database.users);
-});
-
+// Signs in the user.
 app.post('/signin', (req: Request, res: Response) => {
-  if (req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-      } else {
-        res.status(400).json('error logging in');
-      }
+  return db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if (isValid) {
+      db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('unable to get a user'))
+    } else {
+    res.status(400).json('wrong credentials')
+    }
+  })
+  .catch(err => res.status(400).json('wrong credentials'))
 })
 
-/*
-.insert({
-  // If you are using Knex.js version 1.0.0 or higher this 
-  // now returns an array of objects. Therefore, the code goes from:
-  // loginEmail[0] --> this used to return the email
-  // TO
-  // loginEmail[0].email --> this now returns the email
-     email: loginEmail[0].email, // <-- this is the only change!
-     name: name,
-     joined: new Date()
-})
-*/
-
-// This route is registering the user and making a call to the db,
+// This route is registering the user and making a call to the DB,
 // checking whether the user is already registered.
 app.post('/register', (req: Request, res: Response) => {
   const { email, name, password }: IUserForDatabase = req.body;
   const salt = bcrypt.genSaltSync(10);
   const hash = bcrypt.hashSync(password, salt);
-  db('users')
-    .returning('*')
-    .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  })
-  .then(user => {
-    res.json(user[0]);
-  })
-  .catch(err => res.status(400).json('unable to register'))
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 // This is for future installments, for profile page.
@@ -119,12 +96,9 @@ app.get('/profile/:id', (req: Request, res: Response) => {
   .catch(err => res.status(400).json('Not found'))
 })
 
-// Why does req.params return a variable with a distinct type
-// and req.body says that its type is 'any'?
-
 // Updates the rank and increases the count.
 app.put('/image', (req: Request, res: Response) => {
-  const { id } = req.body;
+  const { id }: IUserForDatabase = req.body;
   db('users').where('id', '=', id)
   .increment('entries', 1)
   .returning('entries')
@@ -135,5 +109,5 @@ app.put('/image', (req: Request, res: Response) => {
 })
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
