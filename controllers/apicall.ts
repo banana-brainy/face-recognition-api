a2640cf9b737e18f979b68e23d3230bbadbd4bc0
banana-brainy <krokodeall124@gmail.com>
2023-03-01 14:43:25 +0200

    done face detection function

--- controllers/apicall.ts
+++ controllers/apicall.ts
@@ -1,18 +1,37 @@
 import { Request, Response } from 'express';
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// In this section, we set the user authentication, user and app ID, model details, and the URL
+// of the image we want as an input. Change these strings to run your own example.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = '9969c9d10d3048ecbaf120022d70909b';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'bananabrainy';
+const APP_ID = 'my-first-application';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+/* const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105'; */
+/* const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg'; */
 
 const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 const stub = ClarifaiStub.grpc();
 const metadata = new grpc.Metadata();
-metadata.set("authorization", "Key fae76cb624ef4de9b221e4fbace13a52");
+metadata.set("authorization", "Key 9969c9d10d3048ecbaf120022d70909b");
 
 function handleAPICall({ req, res }: { req: Request; res: Response; })  {
     stub.PostModelOutputs(
         {
-            model_id: "6dc7e46bc9124c5c8824be4822abe105",
-            inputs: [{ data: { image: { url: req.body.input } } }]
+            user_app_id: {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            model_id: MODEL_ID,
+            inputs: [{ data: { image: { url: req.body.input, allow_duplicate_url: true } } }]
         },
         metadata,
-        (err: string, response: { status: { code: number; description: string; details: string; }; outputs: { data: { concepts: any; }; }[]; }) => {
+        (err: string, response: { status: { code: number; description: string; details: string; }; outputs: { data: { concepts: any; regions: any; }; }[]; }) => {
             if (err) {
                 console.log("Error: " + err);
                 return;
@@ -21,11 +40,9 @@ function handleAPICall({ req, res }: { req: Request; res: Response; })  {
                 console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
                 return;
             }
-            console.log("Predicted concepts, with confidence values:");
             for (const c of response.outputs[0].data.concepts) {
                 console.log(c.name + ": " + c.value);
             }
-            console.log(response)
             res.json(response);
         }
     );
