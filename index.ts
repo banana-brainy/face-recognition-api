44bd491ead46488854e2b791a103fff0870badf8
banana-brainy <krokodeall124@gmail.com>
2023-03-14 18:26:11 +0200

    Deleted some sensitive data from the code

--- index.ts
+++ index.ts
@@ -17,9 +17,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'postgres',
-    password: 'Tujh2022!',
-    database: 'face_rec'
+    user: '',
+    password: '',
+    database: ''
   }
 });
 
